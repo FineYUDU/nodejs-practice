375c877904e178f2def6db7a61d164e67c05efb2
Fine <fine_567@hotmail.com>
2024-05-18 14:46:39 -0600

    git ignore amnd winston

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,6 +1,13 @@
-const getPokemonById = require('./js-foundation/06-promises');
+// const getPokemonById = require('./js-foundation/06-promises');
 
-getPokemonById(1)
-    .then((pokemon) => console.log(pokemon))
-    .catch( (err)=> console.log(err))
-    .finally( ()=> console.log('Finalmente'));
+// getPokemonById(1)
+//     .then((pokemon) => console.log(pokemon))
+//     .catch( (err)=> console.log(err))
+//     .finally( ()=> console.log('Finalmente'));
+
+const { buildLogger } = require('./plugins');
+
+const logger = buildLogger('app.js')
+
+logger.log('Hola mundo');
+logger.error('Esto es algo malo');
